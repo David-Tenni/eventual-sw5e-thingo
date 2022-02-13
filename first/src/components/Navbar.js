59356628b86e9f8a4cbc05ecdf3051df03149388
DivadTenni <davidctenni@gmail.com>
2022-02-13 16:26:44 +1100

    refactored: components

--- first/src/components/Navbar.js
+++ first/src/components/Navbar.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-  static defaultProps= {
-    title:'Github Finder',
-    icon: 'fab fa-github',
-  };
+const Navbar = ( {icon, title}) => {
+  return (
+    <nav className='navbar bg-primary'>
+        <h1>
+          <i className={icon} /> {title} 
+          </h1> 
+    </nav>
+  );
+};
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  };
 
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-          <h1>
-            <i className={this.props.icon}></i> navbar</h1> {this.props.title}
-      </nav>
-    )
-  }
-}
+Navbar.defaultProps= {
+  title:'Github Finder',
+  icon: 'fab fa-github',
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 export default Navbar
