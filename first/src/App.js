6066b5bbcb1e15f95a9a82a449a1b1233fff655f
DivadTenni <davidctenni@gmail.com>
2022-02-13 16:47:13 +1100

    Added: now getting data directly from github rather than using palceholder in state

--- first/src/App.js
+++ first/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-componentDidMount() {
-  console.log('s');
+state ={
+  users: [],
+  loading: false,
+}
+
+async componentDidMount() {
+  this.setState({loading: true});
+
+  const res = await axios.get('https://api.github.com/users');
+
+  this.setState({users: res.data, loading: false});
 }
 
   render() {
@@ -13,7 +23,7 @@ componentDidMount() {
         <div className='App'>
             <Navbar/>
             <div className='container'>
-             <Users/>
+             <Users loading={this.state.loading} users={this.state.users} />
             </div>
         </div>
     );
