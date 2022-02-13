a1985891e17b99782331350489e8655be60ebbf5
DivadTenni <davidctenni@gmail.com>
2022-02-13 17:41:10 +1100

    added: use of local env

--- first/src/App.js
+++ first/src/App.js
@@ -12,8 +12,9 @@ state ={
 
 async componentDidMount() {
   this.setState({loading: true});
-
-  const res = await axios.get('https://api.github.com/users');
+  const res = await axios.get(`https://api.github.com/users?
+  client_id=${process.env.REACT_APP_ALUMNI_SEARCH_CLIENT_ID}&
+  client_secret=${process.env.REACT_APP_ALUMNI_SEARCH_SECRET}`);
 
   this.setState({users: res.data, loading: false});
 }
