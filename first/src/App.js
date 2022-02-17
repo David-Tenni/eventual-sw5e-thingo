ce189bf0b21d00d49d1ce5f5bfad4e0e26aa482c
DivadTenni <davidctenni@gmail.com>
2022-02-17 21:28:44 +1100

    updated: destructured some props on appjs

--- first/src/App.js
+++ first/src/App.js
@@ -42,12 +42,13 @@ this.setState({loading: true});
 clearSearch = () => {this.setState({users: [], loading: false})
 };
   render() {
+    const {users, loading} = this.state;
     return (
         <div className='App'>
             <Navbar/>
             <div className='container'>
-              <Search searchUsers={this.searchUsers} clearSearch={this.clearSearch} showClear={this.state.users.length > 0 ? true : false}/>
-             <Users loading={this.state.loading} users={this.state.users} />
+              <Search searchUsers={this.searchUsers} clearSearch={this.clearSearch} showClear={users.length > 0 ? true : false}/>
+             <Users loading={loading} users={users} />
             </div>
         </div>
     );
