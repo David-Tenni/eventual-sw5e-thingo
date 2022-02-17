c2e43542df83b6f54d733999eca14cfa91f7c6dd
DivadTenni <davidctenni@gmail.com>
2022-02-17 17:31:28 +1100

    added: changed to using a github access token as they do not seem to support the prior method anymore for credentials

--- first/src/App.js
+++ first/src/App.js
@@ -12,21 +12,36 @@ state ={
   loading: false,
 }
 
-async componentDidMount() {
-  this.setState({loading: true});
-  const res = await axios.get(`https://api.github.com/users?
-  client_id=${process.env.REACT_APP_ALUMNI_SEARCH_CLIENT_ID}&
-  client_secret=${process.env.REACT_APP_ALUMNI_SEARCH_SECRET}`);
+//testing if getting users from api works
+// async componentDidMount() {
+//   this.setState({loading: true});
+//   const res = await axios.get(`https://api.github.com/users?
+//   client_id=${process.env.REACT_APP_ALUMNI_SEARCH_CLIENT_ID}&
+//   client_secret=${process.env.REACT_APP_ALUMNI_SEARCH_SECRET}`);
 
-  this.setState({users: res.data, loading: false});
-}
+//   this.setState({users: res.data, loading: false});
+// }
+
+//search using this term
+searchUsers = async text => {
+  const res = await axios.get(
+    `https://api.github.com/search/users?q=${text}`,
+    {
+      headers: {
+        Authorization: `${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    }
+  );
+
+  this.setState({users: res.data.items, loading: false});
+};
 
   render() {
     return (
         <div className='App'>
             <Navbar/>
             <div className='container'>
-              <Search></Search>
+              <Search searchUsers={this.searchUsers}/>
              <Users loading={this.state.loading} users={this.state.users} />
             </div>
         </div>
